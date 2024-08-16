b3528ea5b4e741dd076e99149e903b3d09fea3d5
theertha120 <141700972+theertha120@users.noreply.github.com>
2024-08-16 23:04:58 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const faqContainer = document.querySelector('.faq-content');
+
+  faqContainer.addEventListener('click', (e) => {
+    const groupHeader = e.target.closest('.faq-group-header');
+
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector('.faq-group-body');
+    const icon = groupHeader.querySelector('i');
+
+    icon.classList.toggle('fa-plus');
+    icon.classList.toggle('fa-minus');
+
+    groupBody.classList.toggle('open');
+
+    const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+        const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+        otherGroupBody.classList.remove('open');
+        otherIcon.classList.remove('fa-minus');
+        otherIcon.classList.add('fa-plus');
+      }
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerButton = document.querySelector('.hamburger-button');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  hamburgerButton.addEventListener('click', () =>
+    mobileMenu.classList.toggle('active')
+  );
+});
